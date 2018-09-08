8e625284c9975e0244838ff6f255f8521f18e8f7
Bill Langenberg <bill.langenberg@gmail.com>
2018-09-08 17:59:09 -0400

    finished basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+  def setup
+    @user = users(:tester)
+  end
+
+  test "invalid login" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
@@ -9,5 +13,34 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "valid login" do
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+
+    assert t_logged_in?
+  end
+
+  test "valid login followed by logout" do
+    # log in. no further tests needed, as they're covered above.
+    get login_path
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
+    assert t_logged_in?
+
+    # log out behavior
+    delete logout_path
+    assert_not t_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
